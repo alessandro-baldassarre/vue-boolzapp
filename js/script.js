9905548c90b1b7674a21cc9458eb52cc22efc6f0
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-03-21 19:10:00 +0100

    implemented dynamic creation messages current chat

--- js/script.js
+++ js/script.js
@@ -6,7 +6,29 @@ const app = new Vue({
     
     data: {
 
-        activeArray: [
+        activeArray: [ {
+            name: 'Michele',
+            avatar: '_1',
+            visible: true,
+            active: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                }
+            ],
+        },
 
         ],
         
